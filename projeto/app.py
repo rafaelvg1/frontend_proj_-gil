e560fe7c88fd22f45d84710bb0c681c825b8a995
nadapebe <nathandpb@al.insper.edu.br>
2024-11-04 21:48:40 -0300

    ver com tiagao como faz, prb banco dados

--- projeto/app.py
+++ projeto/app.py
@@ -3,10 +3,18 @@ import os
 
 app = Flask(__name__)
 
-# Configura o diretório onde os arquivos HTML e CSS estão
+
+
+# Rota para a página inicial
 @app.route('/')
 def home():
     return render_template('inicial.html')
+
+# Rota para a página de bebidas
+@app.route('/bebidas')
+def bebidas():
+    return render_template('bebidas.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

0eaad36af3e4999694dffd6108bc8e709efe4c7e
rafaelvg1 <rafaelvg1@al.insper.edu.br>
2024-11-04 22:53:31 -0300

    pratos.html, carrinho.html, app.py update

--- projeto/app.py
+++ projeto/app.py
@@ -1,7 +1,30 @@
 from flask import Flask, render_template
 import os
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import os
+
+
 
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb+srv://admin:admin@progeficaz.e4oxl.mongodb.net/insper_pay"
+
+
+mongo = PyMongo(app)
+
+
+@app.route('/', methods=["GET"])
+def teste():
+    filtro={}
+    projecao={"id":0}
+    item=mongo.db.comidas.find(filtro,projecao)
+    return render_template("inicial.html",item=item)
+
+
+
+
+
 
 
 
@@ -15,6 +38,9 @@ def home():
 def bebidas():
     return render_template('bebidas.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000, host='0.0.0.0')
+
+
+
